791d62781d67cb1d187a6b9e9b7e714409e1f087
Jiwon Oh <zionyy@skuniv.ac.kr>
2025-11-01 00:45:17 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 24152 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,28 +1,32 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
-        sc.nextLine();
-
+        int N = Integer.parseInt(br.readLine());
         HashSet<String> set = new HashSet<>();
+
         for (int i = 0; i < N; i++) {
-            set.add(sc.nextLine());
+            set.add(br.readLine());
         }
 
         List<String> list = new ArrayList<>(set);
 
         Collections.sort(list, (a, b) -> {
             if (a.length() == b.length()) {
-                return a.compareTo(b); // 사전순 정렬
+                return a.compareTo(b);
+            } else {
+                return a.length() - b.length();
             }
-            return a.length() - b.length(); // 길이순 정렬
         });
 
         for (String word : list) {
-            System.out.println(word);
+            bw.write(word + "\n");
         }
+        
+        bw.flush();
     }
 }
